cc243f59618da5b662b7d7fd7ef260c51c608ca4
angelo562 <username@example.com>
2022-09-18 16:49:13 -0700

    Formatted document. Changed title variable to entry_title

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,11 +6,9 @@ app_name = "encyclopedia"
 
 urlpatterns = [
     path("", views.index, name="url_index"),
-
     # I can send the submitted query to any path.
     path("search", views.search, name="url_search"),
-    path("<str:title>", views.display, name="url_display"), 
-        # Matches str before ".html"
+    path("<str:entry_title>", views.display, name="url_display"), 
     path("modify", views.create, name="url_modify"),
     
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -9,6 +9,7 @@ import logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger()
 
+
 def index(request):
     """ Returns the home/index page """
     return render(request, "encyclopedia/index.html", {
@@ -16,36 +17,35 @@ def index(request):
         'search_form': SearchForm(),
     })
 
+
 def create(request):
-    """
-    Returns a create/edit page
-    """
+    """    Returns a create/edit page    """
     return render(request, "encyclopedia/modify.html")
 
-def display(request, title):
+
+def display(request, entry_title):
     """ Displays the page for each entry if an entry.md exists"""
-    logger.debug(f"request is {request} of type {type(request)}")
+    logger.info(f"request is {request} of type {type(request)}")
 
     markdowner = Markdown()
-    
-    if util.get_entry(title):
-        return render(request, "encyclopedia/entry.html",{ 
-            "entry": markdowner.convert(util.get_entry(title)),
-            "entry_title": title, #TODO make CSS all capitalized.
+
+    if util.get_entry(entry_title):
+        return render(request, "encyclopedia/entry.html", {
+            "entry": markdowner.convert(util.get_entry(entry_title)),
+            "entry_title": entry_title,  # TODO make CSS all capitalized.
         })
 
-    # Displays error page if entry doesn't exist
     else:
-        return render(request, "encyclopedia/404page.html")
+        return render(request, "encyclopedia/404page.html")  # Error Page
+
 
 def search(request):
     """ Searches for an exact entry  match or returns closest matches if exact entry not found
     """
 
     if request.method == 'GET':
-        GET_value = request.GET.get('q','QueryNotFound')
-        logger.warning(f"request.GET is {request.GET}")
-
+        GET_value = request.GET.get('q', 'QueryNotFound')
+        logger.info(f"request.GET is {request.GET}")
 
         # if an entry exists redirects to "/wiki/{GET_value}"
         if util.get_entry(GET_value):
@@ -55,7 +55,7 @@ def search(request):
             return render(request, "encyclopedia/index.html", {
                 'entries': util.get_close_matches(GET_value),
                 'GET_value': GET_value,
-                'search':True
+                'search': True
             })
 
     return redirect(reverse('encyclopedia:url_index'))
